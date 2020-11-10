f78d713a724ced168fc26edaa3e7976de7b3b2aa
nibble0101 <mj.mawa@yahoo.com>
2020-11-10 16:33:46 +0300

    Started configuring index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
+const Twit = require("twit");
+
+const T = new Twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
+  strictSSL: true,
+});
+
+T.get("search/tweets", { q: "react.js since:2020-11-09", count: 50 }, function (
+  error,
+  data,
+  response
+) {
+    console.log(data);
+});

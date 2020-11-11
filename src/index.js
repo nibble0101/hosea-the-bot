cdc2b2339058b085d4ef285180e149d48a867492
nibble0101 <mj.mawa@yahoo.com>
2020-11-11 16:35:50 +0300

    Added more dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 const Twit = require("twit");
+const express = require("express");
+const { response } = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 const T = new Twit({
   consumer_key: process.env.API_KEY,
@@ -10,10 +15,23 @@ const T = new Twit({
   strictSSL: true,
 });
 
-T.get("search/tweets", { q: "react.js since:2020-11-09", count: 50 }, function (
-  error,
-  data,
-  response
-) {
-    console.log(data)
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
+
+app.get("home/get-tweets/:id", (req, res) => {
+  T.get(
+    "search/tweets",
+    { q: "react.js since:2020-11-09", count: 50 },
+    function (error, data, response) {
+      console.log(data);
+    }
+  );
+});
+app.listen(PORT, () => {
+  console.log(`app listening on  PORT ${PORT}`);
 });

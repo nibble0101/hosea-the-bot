e8a0bac326f9f193e96a25fa962a15925e8e24ce
nibble0101 <mj.mawa@yahoo.com>
2020-11-14 20:29:16 +0300

    Finished styling a list of favorite twitter handles

--- src/favorite_twitter_handles.js
+++ src/favorite_twitter_handles.js
@@ -35,5 +35,16 @@ const favorite_twitter_handles = [
   "@chriscoyier"
 
 ];
-favorite_twitter_handles.sort();
+/*
+This is to ensure proper sorting due to mixed cases in the strings
+Read more at: https://www.digitalocean.com/community/tutorials/js-array-sort-strings
+This sorting is in ascending alphabetical order
+*/
+function sortFavoriteTwitterHandles(a, b) {
+  a = a.toLowerCase();
+  b = b.toLowerCase();
+
+  return a < b ? -1 : b > a ? 1 : 0;
+}
+favorite_twitter_handles.sort(sortFavoriteTwitterHandles);
 module.exports = favorite_twitter_handles;
